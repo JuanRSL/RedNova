d991018c01c45324bdea3f09df48df23051b5e48
Juan <jersrodriguez0@gmail.com>
2026-04-28 16:22:24 -0500

    Cambio a rutas de Post
    Agregado la ruta de los votos del post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+// Ruta para eliminar un post con validación de permisos
 router.delete('/delete/:id', postController.deletePost);
+// Ruta para votar un post
+router.post('/vote', postController.votePost);
 
 module.exports = router;
