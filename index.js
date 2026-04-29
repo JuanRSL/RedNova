bca090b40428729f4bd8e5b2a9ae05ecf80f38fc
LindaVal <lindavalen1109@gmail.com>
2026-04-28 21:52:56 -0500

    Corrección de importaciones en index.js
    Se importaron y usaron las rutas en el index para que el servidor las reconozca

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
+
+// ... después de los middlewares de express
+app.use(express.json()); // ¡Súper importante para recibir datos del body!
+
+// Importar rutas
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+
+// Usar rutas
+app.use('/api/usuarios', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comentarios', commentRoutes);
