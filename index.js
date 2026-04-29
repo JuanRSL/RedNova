917e583e302091940db350d04166db40fa0c446e
LindaVal Q&Q <lindavalen1109@gmail.com>
2026-04-29 03:41:28 -0500

    Creación archivo .env y conexión con BBDD
    Se crea archivo .env para las variables de entorno, se mueve el puerto del  Index al .env y se hace la conexión con la base de datos usando la URI.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express'); // 1. Importar express
-const app = express();              // 2. Crear la instancia de la app
-const PORT = 3000;                  // 3. Definir el puerto
+require('dotenv').config(); // 1. Lee la configuración del archivo .env (Importante antes de traer los datos del archivo)
 
+const express = require('express'); // 2. Importar express
+const app = express();              // 3. Crear la instancia de la app
+const PORT = process.env.PORT || 3000; // 4. Definir el puerto desde archivo .env                  
+
+const mongoose = require('mongoose'); // 5. Conecta con la BBDD
+
+//Conexión al Servidor
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
+
+//Conexión a MongoDB
+mongoose.connect(process.env.URI)
+  .then(()=> console.log("Conectado a BBDD"))
+  .catch(err => console.error("Error al conectar a BBDD", err));
 
 // ... después de los middlewares de express
 app.use(express.json()); // ¡Súper importante para recibir datos del body!
@@ -18,3 +28,4 @@ const commentRoutes = require('./routes/commentRoutes');
 app.use('/api/usuarios', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comentarios', commentRoutes);
+
