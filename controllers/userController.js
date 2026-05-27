727372c6a8bc1d3142a4751f0184d30719527084
Juan <jersrodriguez0@gmail.com>
2026-05-27 10:29:12 -0500

    Cosas menores

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'your_super_secret_key';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Register a new user
 exports.registerUser = async (req, res) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,9 +14,12 @@ router.get('/me', verifyToken , userController.getMyProfile);
 router.put('/me', verifyToken , userController.updateMyProfile);
 
 //SOCIAL: Seguir/Dejar de seguir a otra PERSONA
-router.post('/follow-user', verifyToken, userController.followUser);
+router.post('/users/:id/follow-user', verifyToken, userController.followUser);
 
 //COMUNIDAD: Seguir/Dejar de seguir un SUBFORO
-router.post('/follow-subforum', verifyToken, userController.followSubforum);
+router.post('/subforums/:id/follow-subforum', verifyToken, userController.followSubforum);
+
+//COMUNIDAD: Seguir/Dejar de seguir un FORO
+router.post('/forums/:id/follow-forum', verifyToken, userController.followForum);
 
 module.exports = router;
